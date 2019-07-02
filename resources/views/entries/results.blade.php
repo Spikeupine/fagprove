a7f5fc9df7fb61d550d3a3b1178e69990e5070df
Trygve Sollund <trygve.sollund@cerpus.com>
2019-07-02 18:14:31 +0200

    Remove reply from children
    Remove reply button from children
    
    Make reply button stretched links so you can click anywhere on the card

--- resources/views/entries/results.blade.php
+++ resources/views/entries/results.blade.php
@@ -5,7 +5,9 @@
                 <div class="form-control pb-5 col-12" id="{{ $entry->id }}">{{ $entry->content }}</div>
                 <small class="form-text text-muted">
                     {{ $entry->user()->first()->username.' | ' .$entry->created_at }}
-                 | <a href="{{ route('entry.show', ['entry' => $entry]) }}">reply</a>
+                    @if($entry->parent_id === null)
+                        | <a href="{{ route('entry.show', ['entry' => $entry]) }}" class=" stretched-link">reply</a>
+                    @endif
                 </small>
             </div>
         @endforeach
